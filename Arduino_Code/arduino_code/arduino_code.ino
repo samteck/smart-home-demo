d6aa5c58102b8934b9b99075c5c4cce501a0f61f
Samarth Gupta <samarth.guptas.1995@gmail.com>
2019-06-21 13:04:17 +0530

    added comments to code and modified code

--- Arduino_Code/arduino_code/arduino_code.ino
+++ Arduino_Code/arduino_code/arduino_code.ino
@@ -1,33 +1,38 @@
 /*
- * Project Title: Dangerous Gas Detection
+ * Demo for Havells
  * Author: Samarth Gupta
- * Started Date: 6/7/18
+ * Started Date: 21/6/19
  * Version: 1.0
  */
 /////////////////////////////////////////////////////////////////////////
 // Including the libraries
 #include <dht11.h>
 
+// defining the hardware pins
 #define dhtPin A0
 #define pirPin 7
 
+//creating object for DHT sensor
 dht11 DHT;
 
 void setup() {
-
+  //serial communication at 9600 baud rate
   Serial.begin(9600);
-  delay(1000);
+  
+  //setting pir sensor as INPUT
   pinMode(pirPin,INPUT);
 
 }
 
 void loop() {
-
+    //reading pin values 
     DHT.read(dhtPin);
     boolean pir = digitalRead(pirPin);
-
+    
+    //creating JSON string to send to fog device
     String json = String("{\"pir\":\"") + String(pir) + String("\",\"temp\":\"") + String(DHT.temperature) + String("\",\"humidity\":\"") + String(DHT.humidity) + String("\"}");
+    //writing data on serial bus
     Serial.println(json);
     
-    delay(5000);//Wait 10 sec before accessing sensor again.
+    delay(5000);//Wait 5 sec before accessing sensor again.
 }
--- Raspberry_Pi/fog.py
+++ Raspberry_Pi/fog.py
@@ -1,19 +1,32 @@
-import serial 
-import requests
-import json
-import time
-import RPi.GPIO as IO
+######################
+#Project Title : FOG computing code for Havells Demo
+#Author        : Samarth Gupta
+#Date          : 21/6/2019
+#Version       : 1.0
 
+
+import serial     #serial module to read data on serial bus
+import requests   #request module to send request on cloud
+import json       #to parse and dump json string
+import time       #to wait for some time before sending the request
+import RPi.GPIO as IO #to communicate via GPIO pins for speaker
+
+# setting mode for GPIO pins and disabling intial GPIO warnings
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
+
+# setting up a speaker for output on pin number 3 of GPIO
 speaker = 3
 IO.setup(speaker,IO.OUT)
 
-
+# opening serial port on ACM0 for reading serial data at 9600 baud rate
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+# url for sending request on server
 url = 'https://sam.requestcatcher.com/test'
 
-
+# code below will read data from serial, extract each sensor value, do local fog computing
+# then form a JSON string with room information, and send the request to server
 while 1:
     if(ser.in_waiting >0):
         try:
@@ -24,9 +37,10 @@ while 1:
             temp = json_data["temp"]
             humidity = json_data["humidity"]
 
-            ##add fog computing code
+            ##local fog computing code to start buzzer on intruision
             if (pir == str(0)):
                 IO.output(speaker,IO.HIGH)
+                print("###### ALERT : Intruision Detected #######")
             else:
                 IO.output(speaker,IO.LOW)
          
@@ -36,13 +50,13 @@ while 1:
             data["T"]=str(temp)
             data["H"]=str(humidity)
             json_final = json.dumps(data)
-            print (json_final)
+            print (json_final)   
+        except:
+            print("fail to read")
         
+        try:
             headers = {'Content-type': 'application/json'}
             response = requests.post(url, data=json_final, headers=headers)
             print('Response Code : ' + str(response.status_code))
-
         except:
-            print("fail to read")
-
-
+            print("Server is busy!!!!!")
